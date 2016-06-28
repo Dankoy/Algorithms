632a28c69866a0e595d280119104457b54873488
Evgeny Zelenin <transingularity@gmail.com>
2016-06-28 08:22:07 +0300

    Added new class and separated functionality

--- BinarySearch/src/OrderedArray.java
+++ BinarySearch/src/OrderedArray.java
@@ -1,76 +1,9 @@
-
-class OrdArray {
-	private long[] a; // Ссылка на массив a
-	private int nElems; // Количество элементов данных
-	
-	public OrdArray(int max) { // Конструктор
-		a = new long[max];
-		nElems = 0;
+public class OrderedArray extends OrdArray {
+	public OrderedArray(int max) {
+		super(max);
+		// TODO Auto-generated constructor stub
 	}
 
-	public int size() { 
-		return nElems; 
-	}
-
-	public int find(long searchKey) {
-		int lowerBound = 0;
-		int upperBound = nElems-1;
-		int curIn;
-		while(true)	{
-			curIn = (lowerBound + upperBound ) / 2;
-			if(a[curIn]==searchKey) {    // Элемент найден
-				return curIn; 
-			}
-			else if (lowerBound > upperBound) {   // Элемент не найден
-				return nElems;
-			}
-			else {// Деление диапазона
-				if(a[curIn] < searchKey) {
-				lowerBound = curIn + 1; // В верхней половине
-				}
-				else {
-					upperBound = curIn - 1; // В нижней половине
-				}
-			}
-		}
-	}
-
-	public void insert(long value) { // Вставка элемента в массив
-		int j;
-		for(j=0; j<nElems; j++) // Определение позиции вставки
-			if(a[j] > value) // (линейный поиск)
-				break;
-		for(int k=nElems; k>j; k--) { // Перемещение последующих элементов
-			a[k] = a[k-1]; // Вставка
-		}
-		a[j] = value;
-		nElems++; // Увеличение размера
-	}
-
-	public boolean delete(long value) {
-		int j = find(value);
-		if(j == nElems) { // Найти не удалось
-			return false;
-		}
-		else { // Элемент найден
-			for(int k=j; k<nElems; k++) { // Перемещение последующих элементов
-				a[k] = a[k+1];
-			}
-			nElems--; // Уменьшение размера
-			return true;
-		}
-	}
-
-	public void display() { // Вывод содержимого массива
-		for(int j=0; j<nElems; j++) { // Перебор всех элементов
-			System.out.print(a[j] + " "); // Вывод текущего элемента
-		}
-		System.out.println("");
-	}
-} //OrdArray
-
-
-public class OrderedArray {
 	public static void main(String[] args) {
 		
 		int maxSize = 100; // Размер массива
