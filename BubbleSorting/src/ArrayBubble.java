4eb7ff7c71042ff0a95117107653965e20701be4
Evgeny Zelenin <transingularity@gmail.com>
2016-07-06 16:44:47 +0300

    Finished algorithm. Need to make inserting elements with random numbers.

--- BubbleSorting/src/ArrayBubble.java
+++ BubbleSorting/src/ArrayBubble.java
@@ -26,13 +26,13 @@ public class ArrayBubble {
 		for (out = nElems-1; out > 1; out--){
 			for (in = 0; in < out; in++){
 				if (a[in] > a[in+1]){
-					swap(in, in=1);
+					swap(in, in+1);
 				}
 			}
 		}
 	}
 	
-	private void swap(int one, int two) {
+	private void swap(int one, int two) { 		
 		long temp = a[one];
 		a[one] = a[two];
 		a[two] = temp;
--- BubbleSorting/src/BubbleSort.java
+++ BubbleSorting/src/BubbleSort.java
@@ -1,4 +1,6 @@
 
+/* The example of bubble sorting algorithm. O(N^2) */
+
 public class BubbleSort extends ArrayBubble {
 
 	public BubbleSort(int max) {
@@ -9,10 +11,10 @@ public class BubbleSort extends ArrayBubble {
 		int maxSize = 100; 		// Array size
 		ArrayBubble arr = new ArrayBubble(maxSize);
 		
-		arr.insert(77);
+		arr.insert(77);		// Inserting elements in array . Need to make it randomized.
 		arr.insert(99);
 		arr.insert(44);
-		arr.insert(77); // Вставка 10 элементов
+		arr.insert(77); 	
 		arr.insert(99);
 		arr.insert(66);
 		arr.insert(55);
@@ -23,7 +25,8 @@ public class BubbleSort extends ArrayBubble {
 		arr.insert(66);
 		
 		arr.display();
-
+		arr.bubbleSort();
+		arr.display();
 	}
 
 }
