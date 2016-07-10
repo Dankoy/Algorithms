403768055dd773568ed1a129c7d79d5c8aa53d75
Evgeny Zelenin <transingularity@gmail.com>
2016-07-10 18:43:05 +0300

    Added classes and realization of insertion sort algorithm

---
+++ InsertSorting/src/InsertionSortMain.java
@@ -0,0 +1,24 @@
+
+public class InsertionSortMain extends InsertSortMethods {
+
+	public InsertionSortMain(int max) {
+		super(max);
+	}
+	
+	public static void main(String[] args) {
+		
+		int maxSize = 1000;
+		InsertSortMethods arr = new InsertSortMethods(maxSize);
+		
+		for (int j = 0; j < maxSize; j++) {		// Random insert elements in array
+			long n = (long) (java.lang.Math.random()*(maxSize-1));
+			arr.insert(n);
+		}
+		
+		arr.display();
+		arr.insertionSort();
+		arr.display();
+
+	}
+
+}
