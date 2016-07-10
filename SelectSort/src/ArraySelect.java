9f1da8561769afafad43ac9fdd27d9a232224429
Evgeny Zelenin <transingularity@gmail.com>
2016-07-10 10:23:13 +0300

    Added class ArraySelect

---
+++ SelectSort/src/ArraySelect.java
@@ -0,0 +1,43 @@
+
+public class ArraySelect {
+	
+	private long a[];
+	private int nElems;
+	
+	public ArraySelect(int max) {
+		a = new long[max];
+		nElems = 0;
+	}
+	
+	public void insert(long value) {
+		a[nElems] = value;
+		nElems++;
+	}
+	
+	public void display() {
+		for (int i = 0; i < nElems; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.print(" ");
+	}
+	
+	public void selectionSort() {
+		int in, out, min;
+		for (out = 0; out < nElems; out++) {
+			min = out;
+			for (in = out + 1; in < nElems; in++) {
+				if (a[in] < a[min]) {
+					min = in;
+				}
+				swap(out, min);
+			}
+		}
+	}
+	
+	private void swap(int one, int two) {
+		long tmp = a[one];
+		a[one] = a[two];
+		a[two] = tmp;
+	}
+	
+}
