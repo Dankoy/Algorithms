0044a43d336e6407a8c39515d5013c628c87d251
Evgeny Zelenin <transingularity@gmail.com>
2016-07-16 15:02:12 +0300

    Added main class

---
+++ BracketsStack/src/BracketsMain.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class BracketsMain {
+	
+	public static void main(String args[]) throws IOException {
+		
+		String input;
+		
+		while(true) {
+			System.out.println("Enter brackets string: ");
+			System.out.flush();
+			input = getString();
+			
+			if (input.equals("")) {		// ENTER is typed
+				break;
+			}
+			
+			BracketsChecker bch = new BracketsChecker(input);
+			bch.check();
+		}
+	}
+	
+	public static String getString() throws IOException {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader bf = new BufferedReader(isr);
+		String s = bf.readLine();
+		return s;
+	}
+	
+}
