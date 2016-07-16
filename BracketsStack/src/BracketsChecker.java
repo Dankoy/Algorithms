38e104e2244f4bca89342ef2629f15c8df96d27e
Evgeny Zelenin <transingularity@gmail.com>
2016-07-16 13:31:20 +0300

    Added class BracketsChecker

---
+++ BracketsStack/src/BracketsChecker.java
@@ -0,0 +1,45 @@
+
+public class BracketsChecker {
+	
+	private String input;
+	
+	public BracketsChecker(String in) {		// Constructor
+		input = in;
+	}
+	
+	public void check() {
+		int stackSize = input.length();		// The size of stack equals the size of input string
+		
+		StackX stack = new StackX(stackSize);
+		
+		for (int j = 0; j < stackSize; j++) {
+			char ch = input.charAt(j);
+			switch(ch) {
+				
+				case '{' | '[' | '(':
+					stack.push(ch);
+					break;
+					
+				case '}' | ']' | ')':
+					if( !stack.isEmpty() ) {		// If the stack is not empty
+						char chx = stack.pop(); 	// Pop and check
+						if ( (ch == '}' && chx != '{' ) || (ch == ']' && chx != '[') || (ch == ')' && chx != '(') ) {
+							System.out.println("Error: " + ch + " at " + j);
+						}
+						else {
+							System.out.println("Error: " + ch + " at " + j);
+						}
+						break;
+					}
+					
+				default:		// Ignoring other symbols
+					break;
+			}
+		}
+		
+		if ( !stack.isEmpty() ) { 		// If all symbols are checked but stack is still has elements
+			System.out.println("Error: missing right delimiter");
+		}
+		
+	}
+}
