57114bff1cf1d227d3a3f380912a3fb7e8e1d480
Evgeny Zelenin <transingularity@gmail.com>
2016-07-31 18:15:47 +0300

    Added comments

--- PriorityQueueArray/src/PriorityQueue.java
+++ PriorityQueueArray/src/PriorityQueue.java
@@ -1,5 +1,7 @@
+// The implementation of priority queue based on array (the slower implementation of queue than if it is based on  heap).
+// The lack of queues based on array is slow inserting, but it is appropriate in case if the amount of elements is small and speed of inserting is not really important.
 
-// Elements of array is sorting by key from max (0) to (maxSize-1)
+// Elements of array is sorting by key from max (0) to (maxSize-1). The lower element has higher priority
 
 public class PriorityQueue {
 	
@@ -7,41 +9,41 @@ public class PriorityQueue {
 	private long[] queueArray;
 	private int nItems;
 	
-	public PriorityQueue(int s) {
+	public PriorityQueue(int s) {						// Constructor for queue array
 		maxSize = s;
 		queueArray = new long[maxSize];
 		nItems = 0;
 	}
 	
-	public void insert(long item) {
+	public void insert(long item) {						// Inserting element
 		int j;
 		
-		if (nItems == 0) {
+		if (nItems == 0) {								// If queue is empty then insert element in 0 position
 			queueArray[nItems++] = item;
 		}
-		else {
-			for(j = nItems - 1; j >= 0; j--) {
-				if (item > queueArray[j]) {
-					queueArray[j+1] = queueArray[j];
+		else {											// If queue is not empty  
+			for(j = nItems - 1; j >= 0; j--) {			// then checking elements in reverse way
+				if (item > queueArray[j]) {				// and if the new element is bigger then
+					queueArray[j+1] = queueArray[j];	// move checking element inside queue up
 				}
 				else {
 					break;
 				}
 			}
-			queueArray[j+1] = item;
+			queueArray[j+1] = item;						// Actual inserting of new element
 			nItems++;
 		}
 	}
 	
-	public long remove() {
+	public long remove() {								// Removing minimal element
 		return queueArray[--nItems];
 	}
 	
-	public long peekMain() {
+	public long peekMain() {							// Reading minimal element
 		return queueArray[nItems-1];
 	}
 	
-	public boolean isEmpty() {
+	public boolean isEmpty() {	
 		return (nItems == 0);
 	}
 	
